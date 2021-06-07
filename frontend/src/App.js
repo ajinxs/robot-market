3ef25a7e886fcc658ef54c3bd3243fced04672ee
ajinxs <leeajink@gmail.com>
2021-06-07 10:20:16 +0700

    Basic outline for the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import Section from './components/Section';
 
 function App() {
   return (
     <div className="App">
       <h1>Robot Market</h1>
+
+      <Router>
+        <Header/>
+        <Section/>
+      </Router>
+
       {/*Add your code here*/}
     </div>
   );
